bc5cae811fd46b729fdcf755f7d43b32715f31fe
Sonal-Builds <sonalkarakkunnel@gmail.com>
2025-06-25 22:13:36 +0530

    modified

--- Factorial.js
+++ Factorial.js
@@ -1,13 +1,26 @@
-//Big-O = O(n)
+//Time Complexity: O(n)
+//Space Complexity: O(1)
 function Factorial(n) {
     let result = 1
-    for (i = 2; i <= n; i++) {
-        result *= i
-        console.log(`${i} * ${result / i} = ${result}`)
+    for(i=1; i<=n; i++ ) {
+        result *=  i;
     }
-    return result
+    return `${n}! = ${result}`;
 }
 
 console.log(Factorial(0))
 console.log(Factorial(1))
-console.log(Factorial(4))
+console.log(Factorial(2))
+console.log(Factorial(100)) 
+
+function FactorialRecuesive(n) {
+    if (n === 0 || n === 1) return 1;
+    return n * FactorialRecuesive(n-1)
+}
+
+console.log(FactorialRecuesive(0))
+console.log(FactorialRecuesive(1))
+console.log(FactorialRecuesive(2))
+console.log(FactorialRecuesive(100))
+
+
--- Fibonacci_Sequence.js
+++ Fibonacci_Sequence.js
@@ -1,16 +1,43 @@
-//Big-O = O(n)
-function FibonacciSequence(n) {   
-    let arr = [0, 1]
-    if (n == 2) return arr
-    if (n < 2) return console.log("No value for this")
 
-    for (i = 2; i < n; i++) {
-        arr[i] = arr[i - 1] + arr[i - 2]
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+function fibonacci(n) {
+    let fib = [0,1]
+    if(n < 0) return 'Not Valid';
+    if(n < 2) return fib[n];
+    for(i=2; i<=n; i++) {
+        fib[i] = fib[i-1] +fib[i-2]
     }
-    return arr
+    return fib[n]
 }
-// console.log(FibonacciSequence(2))
-// console.log(FibonacciSequence(0))
-// console.log(FibonacciSequence(10))
-// console.log(FibonacciSequence(-5))
-// console.log(FibonacciSequence(4.5))
+console.log(fibonacci(-1))
+console.log(fibonacci(0))
+console.log(fibonacci(1))
+console.log(fibonacci(2))
+console.log(fibonacci(3))
+
+
+
+
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+function fibonacci2(n) {
+    if (n < 0) return 'invalid'
+    
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    let prev = 0, curr = 1;
+
+    for (let i = 2; i <= n; i++) {
+        [prev, curr] = [curr, prev + curr];
+    }
+
+    return curr;
+}
+
+console.log(fibonacci2(-1))
+console.log(fibonacci2(0))
+console.log(fibonacci2(1))
+console.log(fibonacci2(2))
+console.log(fibonacci2(3))
