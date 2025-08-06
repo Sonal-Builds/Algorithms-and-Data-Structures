398f630507edc77617a3616a1458b09d693d1f0e
Sonal-Builds <sonalkarakkunnel@gmail.com>
2025-08-06 23:24:57 +0530

    Number of 1 Bits

--- Array/RotateArray.js
+++ Array/RotateArray.js
@@ -1,11 +1,32 @@
-var rotate = function(nums, k) {
+function rotate(nums, k) {
+    const n = nums.length;
+    k = k % n;
 
-    for(i=1;i<=k;i++) {
-        let lastValue = nums[nums.length - 1]
-        for(j=nums.length - 2;j>=0;j--) {
-            nums[j+1] = nums[j]
+    for (let i = 0; i < k; i++) {
+        const last = nums[n - 1];
+
+        for (let j = n - 1; j > 0; j--) {
+            nums[j] = nums[j - 1];
         }
-        nums[0] = lastValue
+
+        nums[0] = last;
     }
-    
-};
+}
+
+function rotate(nums, k) {
+    const n = nums.length;
+    k = k % n;
+
+    const reverse = (start, end) => {
+        while (start < end) {
+            [nums[start], nums[end]] = [nums[end], nums[start]];
+            start++;
+            end--;
+        }
+    };
+
+    reverse(0, n - 1);     // Reverse the whole array
+    reverse(0, k - 1);     // Reverse the first k elements
+    reverse(k, n - 1);     // Reverse the rest
+}
+
